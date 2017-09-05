f4359764aa58e8872b97d7aae92c7b5298d29c65
shenwei <1903851304@qq.com>
2017-09-05 23:38:23 +0800

    update

--- LinuxDemo/tcp_server.c
+++ LinuxDemo/tcp_server.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	serv_addr.sin_port = htons(atoi(argv[1]));
 
-	if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+	if ( bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1 )
 		error_handling("bind() error!");
 
 	if (listen(serv_sock, 5) == -1)
