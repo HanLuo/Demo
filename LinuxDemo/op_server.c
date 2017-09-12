bb4a3920c8b68c4abcac61e64096291b39d2155f
wshengrd_qiyun <1903851304@qq.com>
2017-09-12 16:03:45 +0800

    fix bug

--- LinuxDemo/op_server.c
+++ LinuxDemo/op_server.c
@@ -6,15 +6,23 @@
 #include <sys/socket.h>
 
 #define BUF_SIZE 1024
+#define OPSZ 4
 
 void error_handling(char *message);
+
+int calculate(int opnum, int opnds[], char oprator);
 
 int main(int argc, char *argv[])
 {
 	int serv_sock, clnt_sock;
 	struct sockaddr_in serv_addr, clnt_addr;
 
-	char message[BUF_SIZE];
+	char opinfo[BUF_SIZE];
+
+	socklen_t clnt_sock_size;
+
+	int result, opnd_cnt, i;
+	int recv_cnt, recv_len;
 
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
 	if (serv_sock == -1)
@@ -33,8 +41,26 @@ int main(int argc, char *argv[])
 
 	while(1)
 	{
-		
+		clnt_sock_size = sizeof(clnt_addr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_sock_size);
+		if (clnt_sock == -1)
+			error_handling("accept() error!");
+
+		read(clnt_sock, &opnd_cnt, 1);
+
+		printf("num++++++++++: %d\n", opnd_cnt);
+		recv_len = 0;
+		while((opnd_cnt*OPSZ+1) > recv_len)
+		{
+			recv_cnt = read(clnt_sock, &opinfo[recv_len], BUF_SIZE-1);
+			recv_len += recv_cnt;
+		}
+		result = calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len-1]);
+		write(clnt_sock, (char*)&result, sizeof(result));
+		close(clnt_sock);
 	}
+	close(serv_sock);
+	return 0;
 }
 
 void error_handling(char *message)
@@ -42,4 +68,27 @@ void error_handling(char *message)
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
+}
+
+int calculate(int opnum, int opnds[], char op)
+{
+	int result = opnds[0], i;
+	switch(op)
+	{
+	case '+':
+		for (i = 1; i < opnum; i++)
+			result += opnds[i];
+		break;
+	case '-':
+		for (i = 1; i < opnum; i++)
+			result -= opnds[i];
+		break;
+	case '*':
+		for (i = 1; i < opnum; i++)
+			result *= opnds[i];
+		break;
+	default:
+		break; 
+	}
+	return result;
 }
